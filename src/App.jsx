717a66efe0300d6ac073023326446c179bb54fba
Ashar Khan <153488715+Asharkhan468@users.noreply.github.com>
2025-01-25 16:19:01 +0500

    FRONTEND HACKATHON [ADD]

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+
+
+
+const App = () => {
+
+  return (
+    <>
+    
+    <Navbar/>
+    <Outlet/>
+
+  
+    
+    </>
+  );
+};
+
+export default App;
+
+
+
+
+
